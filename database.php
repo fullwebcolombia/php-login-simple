7005cd33b2928551757dc9240283f2f8ae3f34ad
Fullweb Colombia <46874516+fullwebcolombia@users.noreply.github.com>
2019-02-01 00:59:21 -0500

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = 'localhost:3307';
-$username = 'root';
-$password = '';
-$database = 'php_login_database';
+$server = 'fdb24.awardspace.net:3306';
+$username = '2918174_fullweb';
+$password = 'fullwebcolombia2015';
+$database = '2918174_fullweb';
 
 try {
   $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
